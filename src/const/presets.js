ca6c7469f07855cd056c4f91d5d2a81d8ab930e1
Ruiqi Mao <ruiqim@gmail.com>
2017-01-11 12:10:35 -0800

    added S60-X presets

--- src/const/presets.js
+++ src/const/presets.js
@@ -3,5 +3,8 @@ module.exports = {
 	'alps64-aek': 'Alps64 (AEK)',
 	'minivan-std': 'MiniVan (Standard)',
 	'minivan-arrow': 'MiniVan (Arrow)',
+	's60x-rgb-ansi': 'S60-X RGB (ANSI)',
+	's60x-rgb-iso': 'S60-X RGB (ISO)',
+	's60x-rgb-generic': 'S60-X RGB (Generic)',
 	'alps44': 'Alps44'
 };
