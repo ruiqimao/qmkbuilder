abd7789967d4880a8f4e45fa89326c5dbbfd8239
Ruiqi Mao <ruiqim@gmail.com>
2017-01-12 23:48:18 -0800

    add JD45 preset to code

--- src/const/presets.js
+++ src/const/presets.js
@@ -7,5 +7,6 @@ module.exports = {
 	's60x-rgb-ansi': 'S60-X RGB (ANSI)',
 	's60x-rgb-iso': 'S60-X RGB (ISO)',
 	's60x-rgb-generic': 'S60-X RGB (Generic)',
+	'jd45-default': 'JD45',
 	'alps44': 'Alps44'
 };
