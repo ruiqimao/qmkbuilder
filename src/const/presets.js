bc3acc1d815a4c3a6bed6bcb9c9cd31d1452defa
Ruiqi Mao <ruiqim@gmail.com>
2019-04-18 23:02:57 -0400

    add more presets

--- src/const/presets.js
+++ src/const/presets.js
@@ -4,6 +4,10 @@ module.exports = {
 	'alps64-aek': 'Alps64 (AEK)',
 	'minivan-std': 'MiniVan (Standard)',
 	'minivan-arrow': 'MiniVan (Arrow)',
+	'1up-rgb-custom-pcb-full-map': '1Up RGB Custom PCB (Full)',
+	'1up-rgb-custom-pcb-standard-map': '1Up RGB Custom PCB (Standard)',
+    '1up-rgb-hse-standard': '1Up RGB HSE PCB (Standard)',
+	'1up-sweet16': '1Up Sweet16',
 	's60x-rgb-ansi': 'S60-X RGB (ANSI)',
 	's60x-rgb-iso': 'S60-X RGB (ISO)',
 	's60x-rgb-generic': 'S60-X RGB (Generic)',
@@ -11,5 +15,7 @@ module.exports = {
 	'jd45-default': 'JD45',
 	'bantam-pcb': 'Bantam44 (PCB)',
 	'alps44': 'Alps44',
-	'miuni32-rgb-standard': 'Miuni32 RGB (Standard)'
+	'miuni32-rgb-standard': 'Miuni32 RGB (Standard)',
+    'treasure-type9-default': 'Treasure Macropad Type-9',
+    'zlant-default': 'Zlant'
 };
