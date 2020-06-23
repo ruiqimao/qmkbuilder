813208ec772277e46fdd8e9efbe9d9857e684d0e
Ruiqi <ruiqim@gmail.com>
2020-06-23 00:16:36 +0000

    Minor changes for new Bazel/Docker envrionments
    Removed extraneous files with spaces in names
    Changed server to serve static content
    Changed server to be configured with environment variables
    Changed server to handle SIGINT and SIGTERM

--- server/index.js
+++ server/index.js
@@ -1,6 +1,3 @@
-const PORT = 5004;
-const TMP = '/tmp/qmk-';
-
 const Express = require('express');
 const BodyParser = require('body-parser');
 const Crypto = require('crypto');
@@ -8,6 +5,20 @@ const Exec = require('child_process').exec;
 const Fs = require('fs');
 
 const co = require('co');
+
+// Get environment variables.
+const envTMP = process.env.TMP || '/tmp/qmk-';
+const envPORT = process.env.PORT || 80;
+const envQMK = process.env.QMK || null;
+const envSTATIC = process.env.STATIC || null;
+if (envQMK === null) {
+  console.error('No QMK environment variable specified');
+  process.exit(1);
+}
+if (envSTATIC === null) {
+  console.error('No STATIC environment variable specified');
+  process.exit(1);
+}
 
 // Create the express app.
 const app = Express();
@@ -22,29 +33,35 @@ app.all('*', (req, res, next) => {
 	next();
 });
 
+// Serve static content.
+app.use(Express.static(envSTATIC));
+
 // Set up the /build route.
 app.post('/build', (req, res) => {
 	// Get the files.
 	const files = req.body;
 
-	// Create a random key.
+	// Create a temporary directory.
 	const key = Crypto.randomBytes(16).toString('hex');
+  const tmpdir = envTMP + key;
 
 	// Setup helper functions.
 	const clean = () => {
-		Exec('rm -rf ' + TMP + key);
+		Exec('rm -rf ' + tmpdir);
 	};
 
 	const sendError = err => {
+    console.error(err);
 		res.json({ error: err });
 		clean();
 	};
 
 	// Start.
 	co(function*() {
+
 		// Copy the base stencil.
 		yield new Promise((resolve, reject) => {
-			Exec('cp -rp qmk_firmware ' + TMP + key, (err, stdout, stderr) => {
+			Exec('cp -rp ' + envQMK + ' ' + tmpdir, (err, stdout, stderr) => {
 				if (err) return reject('Failed to initialize.');
 				resolve();
 			});
@@ -53,7 +70,7 @@ app.post('/build', (req, res) => {
 		// Copy all the files.
 		for (const file in files) {
 			yield new Promise((resolve, reject) => {
-				const fileName = file.replace('qmk_firmware', TMP + key);
+				const fileName = file.replace('qmk_firmware', tmpdir);
 				Fs.writeFile(fileName, files[file], err => {
 					if (err) return reject('Failed to initialize.');
 					resolve();
@@ -63,7 +80,7 @@ app.post('/build', (req, res) => {
 
 		// Make.
 		yield new Promise((resolve, reject) => {
-			Exec('cd ' + TMP + key + '/keyboards/kb && make', (err, stdout, stderr) => {
+			Exec('cd ' + tmpdir + '/keyboards/kb && make', (err, stdout, stderr) => {
 				if (err) return reject(stderr);
 				resolve();
 			});
@@ -71,7 +88,7 @@ app.post('/build', (req, res) => {
 
 		// Read the hex file.
 		const hex = yield new Promise((resolve, reject) => {
-			Fs.readFile(TMP + key + '/kb_default.hex', 'utf8', (err, data) => {
+			Fs.readFile(tmpdir + '/kb_default.hex', 'utf8', (err, data) => {
 				if (err) return reject('Failed to read hex file.');
 				resolve(data);
 			});
@@ -86,4 +103,12 @@ app.post('/build', (req, res) => {
 });
 
 // Start listening.
-app.listen(PORT, () => console.log('Listening on port ' + PORT + '...'));
+app.listen(envPORT, () => console.log('Listening on port ' + envPORT + '...'));
+
+// Exit on SIGINT and SIGTERM.
+process.on('SIGTERM', shutdown);
+process.on('SIGINT', shutdown);
+function shutdown() {
+  // TODO: This is not a clean shutdown.
+  process.exit(0);
+}
